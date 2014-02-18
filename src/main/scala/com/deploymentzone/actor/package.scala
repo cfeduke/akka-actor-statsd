8913d72cc93181f7578050d9999b63a7e065140d
Charles Feduke <charles.feduke@gmail.com>
2014-02-18 15:00:37 -0500

    added package for actor with type aliases

---
+++ src/main/scala/com/deploymentzone/actor/package.scala
@@ -0,0 +1,9 @@
+package com.deploymentzone
+
+package object actor {
+  type Count = protocol.Count
+  type Increment = protocol.Increment
+  type Decrement = protocol.Decrement
+  type Gauge = protocol.Gauge
+  type Timing = protocol.Timing
+}
