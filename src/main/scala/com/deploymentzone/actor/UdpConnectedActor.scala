6373be5d938a21bf4df4232e1e61506ea6191159
Charles Feduke <charles.feduke@gmail.com>
2014-02-18 11:45:14 -0500

    note about using a connected UDP

--- src/main/scala/com/deploymentzone/actor/UdpConnectedActor.scala
+++ src/main/scala/com/deploymentzone/actor/UdpConnectedActor.scala
@@ -6,6 +6,8 @@ import akka.io.{UdpConnected, IO}
 import akka.util.ByteString
 
 /* originated from: http://doc.akka.io/docs/akka/snapshot/scala/io-udp.html */
+
+/* by using a connected form instead of a simple sender, security checks are cached instead of verified on every send */
 private[deploymentzone] class UdpConnectedActor(remote: InetSocketAddress, requester: ActorRef)
   extends Actor
   with ActorLogging {
