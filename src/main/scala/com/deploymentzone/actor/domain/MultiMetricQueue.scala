973dc18a000872b5858ce0b4f8021ab5a76724cd
Charles Feduke <charles.feduke@gmail.com>
2014-02-20 15:53:06 -0500

    refactored if branching to case

--- src/main/scala/com/deploymentzone/actor/domain/MultiMetricQueue.scala
+++ src/main/scala/com/deploymentzone/actor/domain/MultiMetricQueue.scala
@@ -55,33 +55,34 @@ private[actor] class MultiMetricQueue(val packetSize: Int)(implicit system: Acto
   def payload(): String = {
     @tailrec
     def recurse(acc: StringBuilder = new StringBuilder, utf8Length: Int = 0): String = {
-      val utf8 = Charset.forName("utf-8")
+
+      val UTF8 = Charset.forName("utf-8")
       queue.isEmpty match {
         case true => acc.toString()
         case false =>
-          val proposedAddition = queue.head.getBytes(utf8).length
-          if (proposedAddition > packetSize) {
-            if (logger.isWarningEnabled) {
-              val DISCARD_MSG_MAX_LENGTH = 25
-              val discardMsgLength = queue.head.length
-              val ellipsis = discardMsgLength match {
-                case n if n > DISCARD_MSG_MAX_LENGTH => "..."
-                case _ => ""
+          queue.head.getBytes(UTF8).length match {
+            case proposedAddition if proposedAddition > packetSize =>
+              if (logger.isWarningEnabled) {
+                val DISCARD_MSG_MAX_LENGTH = 25
+                val discardMsgLength = queue.head.length
+                val ellipsis = discardMsgLength match {
+                  case n if n > DISCARD_MSG_MAX_LENGTH => "..."
+                  case _ => ""
+                }
+                val discardMsg = queue.head.substring(0, Math.min(DISCARD_MSG_MAX_LENGTH, discardMsgLength)) + ellipsis
+                logger.warning(s"""Message "$discardMsg" discarded because its size ($discardMsgLength) was larger than the permitted maximum packet size ($packetSize)""")
               }
-              val discardMsg = queue.head.substring(0, Math.min(DISCARD_MSG_MAX_LENGTH, discardMsgLength)) + ellipsis
-              logger.warning(s"""Message "$discardMsg" discarded because its size ($discardMsgLength) was larger than the permitted maximum packet size ($packetSize)""")
-            }
-            val (_, sq) = queue.dequeue
-            queue = sq
-            recurse(acc, utf8Length)
-          } else if (proposedAddition + utf8Length + 1 > (packetSize + 1)) {
-            acc.toString()
-          } else {
-            val (item, sq) = queue.dequeue
-            queue = sq
-            acc.append(item)
-            acc.append("\n")
-            recurse(acc, proposedAddition + utf8Length)
+              val (_, sq) = queue.dequeue
+              queue = sq
+              recurse(acc, utf8Length)
+            case proposedAddition if proposedAddition + utf8Length + 1 > (packetSize + 1) =>
+              acc.toString()
+            case proposedAddition =>
+              val (item, sq) = queue.dequeue
+              queue = sq
+              acc.append(item)
+              acc.append("\n")
+              recurse(acc, proposedAddition + utf8Length)
           }
       }
     }
