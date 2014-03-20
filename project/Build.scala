c8277f247c3206b9ce52c14a16e60ad5cf4d3f0d
Charles Feduke <charles.feduke@gmail.com>
2014-03-20 14:19:36 -0400

    attempting akka 2.2.3 with testkit 2.3.0

--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,7 @@ object Build extends sbt.Build {
     settings = Project.defaultSettings ++ Seq(
       name                  := "akka-actor-statsd",
       organization          := "com.deploymentzone",
-      version               := "0.2-SNAPSHOT",
+      version               := "0.3-SNAPSHOT",
       licenses              := Seq("MIT" -> url("http://opensource.org/licenses/MIT")),
       homepage              := Some(url("https://github.com/cfeduke/akka-actor-statsd/")),
       scalaVersion          := "2.10.3",
@@ -42,7 +42,8 @@ object Build extends sbt.Build {
   object Dependencies {
 
     object Versions {
-      val akka              = "2.3.0"
+      val akka              = "2.2.3"
+      val akkaTestKit       = "2.3.0"
       val scalatest         = "2.0"
       val logback           = "1.0.13"
       val ficus             = "1.0.0"
@@ -56,8 +57,8 @@ object Build extends sbt.Build {
     )
 
     val testDependencies = Seq(
-      "com.typesafe.akka"   %% "akka-testkit"     % Versions.akka       % "test",
-      "org.scalatest"       %% "scalatest"        % Versions.scalatest  % "test"
+      "com.typesafe.akka"   %% "akka-testkit"     % Versions.akkaTestKit  % "test",
+      "org.scalatest"       %% "scalatest"        % Versions.scalatest    % "test"
     )
 
     def apply(): Seq[ModuleID] = compileDependencies ++ testDependencies
